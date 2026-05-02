b7c29253d215008de3975875d76168ecf6442b2c
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-05-02 11:13:35 +0000

    refactor: configure apps in main settings

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    # main apps
+    "apps.core",
+    "apps.accounts",
+    "apps.processing",
+    "apps.media",
+    "apps.analytics",
 ]
 
 MIDDLEWARE = [
