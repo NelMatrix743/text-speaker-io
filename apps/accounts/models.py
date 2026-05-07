1ba99a96cf6635841da167478a6941ac186eeae2
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-05-07 11:29:46 +0000

    feat(models): define account data model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,13 +1,23 @@
-from django.db import models
+from django.db import models as md
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
+
+
+class AccountStatus(md.TextChoices):
+    """ Account status choices """
+
+    ACTIVE = ("active", "Active")
+    EXPIRED = ("expired", "Expired")
+    CANCELLED = ("cancelled", "Cancelled")
 
 
 class User(AbstractUser):
-    email: models.EmailField = models.EmailField(unique=True)
+    """ Represents an authenticated user of the platform """
+    email: md.EmailField = md.EmailField(unique=True)
 
-    created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True)
-    updated_at: models.DateTimeField = models.DateTimeField(auto_now=True)
+    created_at: md.DateTimeField = md.DateTimeField(auto_now_add=True)
+    updated_at: md.DateTimeField = md.DateTimeField(auto_now=True)
 
     USERNAME_FIELD: str = "email" # use email for login
     REQUIRED_FIELDS: list[str] = [
@@ -18,9 +28,44 @@ class User(AbstractUser):
         return f"EMAIL({self.email}); USERNAME({self.username})"
 
 
-class Account(models.Model):
-    pass
+
+class Account(md.Model):
+    """ Represents a user's subscription and usage """
+
+    user: md.OneToOneField = md.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=md.CASCADE,
+        related_name="account"
+    )
+    plan: md.ForeignKey = md.ForeignKey(
+        "Plan",
+        on_delete=md.CASCADE,
+        related_name="accounts"
+    )
+
+    status: md.CharField = md.CharField(
+        max_length=10,
+        choices=AccountStatus.choices,
+        default=AccountStatus.ACTIVE.value
+    )
+
+    # subscription date
+    sub_start_date: md.DateTimeField = md.DateTimeField()
+    sub_end_date: md.DateTimeField = md.DateTimeField(null=True, blank=True)
+
+    # usage metrics
+    total_input_created: md.IntegerField = md.IntegerField(default=0)
+    total_operations_generated: md.IntegerField = md.IntegerField(default=0)
+    total_output_generated: md.IntegerField = md.IntegerField(default=0)
+    total_storage_used: md.FloatField = md.FloatField(default=0.0)
+    
+    created_at: md.DateTimeField = md.DateTimeField(auto_now_add=True)
+    updated: md.DateTimeField = md.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Account #{self.id} - {self.user}"
 
 
-class Plan(models.Model):
+
+class Plan(md.Model):
     pass
