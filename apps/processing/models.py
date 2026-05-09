6de2a2a28f8e5931ffd04acb498188e2dca3f51d
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-05-09 21:45:50 +0000

    feat(models): define the Output data model

--- apps/processing/models.py
+++ apps/processing/models.py
@@ -10,6 +10,13 @@ class InputType(md.TextChoices):
     URL = ("url", "URL")
 
 
+class OutputType(md.TextChoices):
+    """ Output type choices """
+
+    TEXT = ("text", "Text")
+    FILE = ("file", "File")
+
+
 class OperationStatus(md.TextChoices):
     """ Operation status choices """
 
@@ -63,7 +70,7 @@ class Operation(md.Model):
     input: md.ForeignKey = md.ForeignKey(
         "Input",
         on_delete=md.CASCADE,
-        related_name="input"
+        related_name="operations"
     )
 
     created_at: md.DateTimeField = md.DateTimeField(auto_now_add=True)
@@ -74,4 +81,36 @@ class Operation(md.Model):
 
 
 class Output(md.Model):
-    pass
+    """ Represents a result generated from an operation """
+
+    output_type: md.CharField = md.CharField(
+        max_length=100,
+        choices=OutputType,
+        default=OutputType.FILE.value
+    )
+
+    operation: md.OneToOneField = md.OneToOneField(
+        "Operation",
+        on_delete=md.CASCADE,
+        related_name="output"
+    )
+
+    raw_text: md.TextField = md.TextField(null=True)
+    media_file: md.ForeignKey = md.ForeignKey(
+        "apps.media.MediaFile",
+        on_delete=md.CASCADE,
+        related_name="output",
+        null=True
+    )
+
+    text_length: md.PositiveIntegerField = md.PositiveIntegerField(
+        null=True
+    )
+    audio_duration: md.PositiveIntegerField = md.PositiveIntegerField(
+        null=True
+    ) # if audio file output, save duration in seconds
+
+    created_at: md.DateTimeField = md.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.operation.operation_type} - {self.output_type}"
