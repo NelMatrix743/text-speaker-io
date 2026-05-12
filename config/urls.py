a439e4148e4948304f9b6644f5c1b6c61bf376b6
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-05-12 20:33:22 +0000

    feat: map main apps in main urls config module

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.urls.resolvers import URLResolver
 
-urlpatterns = [
+urlpatterns: list[URLResolver] = [
     path('admin/', admin.site.urls),
+
+    # app url confs
+    path('', include("apps.core.urls")),
+    path("auth/", include("apps.accounts.urls")),
+    path("process/", include("apps.processing.urls"))
+
 ]
