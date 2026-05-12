a56c69409367de130f648808a1dbe5724c53b898
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-05-12 21:04:05 +0000

    feat(logic): define views for index and home page

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, "core/index.html")
+
+
+def home(request: HttpRequest) -> HttpResponse:
+    return render(request, "core/home.html")
